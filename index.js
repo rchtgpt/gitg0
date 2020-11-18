0cea876bde666b537e82e9b001eda95841b3f084
Rachit Gupta <guptarachit2004@gmail.com>
2020-11-18 18:00:59 +0530

    :sparkles: cli: Add github issue title fetcher

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const cowsay = require('cowsay');
 const files = require('./lib/files.js');
-const remoteUrl = require('./lib/remoteRepoLink.js');
-const { getCurrentDirectoryGitRemoteUrl } = require('./lib/remoteRepoLink.js');
+const { getQuestions } = require('./lib/inquirer.js');
 
 clear();
 
-// prints tool name on start
+// displays Gitgo on start
 if (files.directoryExists('.git')) {
     console.log(figlet.textSync('Gitgo', {
         horizontalLayout: 'default',
         verticalLayout: 'default',
-    }));
-    getCurrentDirectoryGitRemoteUrl();
+    }), '\n');
+    // asks task based questions
+    getQuestions();
 } else {
+    // checks if the directory is a git based repo or not
     console.log(cowsay.say({
         text: 'Not a git repository!',
-        T: "U "
+        T: 'U '
     }
     ));
     process.exit();
