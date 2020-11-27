f3de4319f56588080789a7d9a1ffb5b927c1caa3
Preet Shah <shahpreetk@gmail.com>
2020-11-27 21:31:23 +0530

    Test made for Commit Suggestion (#57)
    * 📦 cli: refactoring code with functions having return value
    
    * :rocket: cli: test for function suggestCommitMsgCb pass
    
    * :sparkles: cli: decreased timeout
    
    * 💄general: Beautify files
    
    Co-authored-by: agg-shambhavi <agg.shambhavi@gmail.com>

---
+++ test/commit-gen.test.js
@@ -0,0 +1,13 @@
+const reader = require("../lib/funcs/jsonReader");
+const {
+  suggestCommitMsgCb
+} = require('../lib/funcs/commit-gen')
+
+test('Suggests Commit Message Callback', () => {
+  reader.jsonReader('../.gitgo', (err, conf) => {
+    if (err) {
+      return
+    }
+    expect(suggestCommitMsgCb(conf).not.toBe(undefined))
+  })
+})
