67c994b905721c5888ff57d2ca09dddb2c2a72c9
Preet Shah <shahpreetk@gmail.com>
2020-11-27 20:31:47 +0530

    :sparkles: cli: decreased timeout

--- test/commit-gen.test.js
+++ test/commit-gen.test.js
@@ -1,14 +1,11 @@
 const reader = require("../lib/funcs/jsonReader");
-const {
-  suggestCommitMsgCb,
-  suggestCommitMsg
-} = require('../lib/funcs/commit-gen')
+const {suggestCommitMsgCb} = require('../lib/funcs/commit-gen')
 
 test('Suggests Commit Message Callback', () => {
-  reader.jsonReader('../.gitgo', (err, conf) => {
-    if (err) {
-      return
-    }
-    expect(suggestCommitMsgCb(conf).not.toBe(undefined))
-  })
+    reader.jsonReader('../.gitgo', (err, conf) => {
+        if (err) {
+          return
+        }
+        expect(suggestCommitMsgCb(conf).not.toBe(undefined))
+      })
 })
